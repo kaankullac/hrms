c4aa0d32fe900758ca155d6cf3ae1b6ad8252327
KaanKullac <KaanKullac@DESKTOP-52V7RI0>
2021-05-30 00:48:42 +0300

    .

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -0,0 +1,34 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.JobtitleService;
+import kodlamaio.hrms.entities.concretes.JobTitles;
+
+
+@RestController
+@RequestMapping("/api/jobtitles")
+public class JobTitlesController {
+	
+	
+	
+	private JobtitleService jobTitleService;
+	
+	@Autowired
+	public JobTitlesController(JobtitleService jobTitleService) {
+		super();
+		this.jobTitleService = jobTitleService;
+	}
+
+	@GetMapping("/getall")
+	
+	public List<JobTitles> getAll(){
+		
+		return this.jobTitleService.getAll();
+	}
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitles.java
@@ -0,0 +1,25 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="jobtitles")
+@Data
+public class JobTitles {
+		
+		@Id
+		@GeneratedValue
+		@Column(name="id")
+		private int id;
+		
+		@Column(name="job_title")
+		private String jobTitle;
+		
+		
+}
